6a6fedb7c0decf6d0b64742c53dcb4674d3f816a
nitinEMA <Nitin@ema.co>
2024-01-31 20:37:08 +0530

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import * as fs from 'fs';
+
+const configFile = fs.readFileSync('config.json', 'utf-8');
+const config = JSON.parse(configFile);
+
+const username: string = config["username"];
+const accountName: string = config["accountName"];
+const buttonName: string = `${accountName} ${username}`;
+
+test('login', async ({ browser }) => {
+  const context = await browser.newContext({ storageState: 'state.json' });
+  const page = await context.newPage();
+  await page.goto('https://dev.ema.co/');
+  await page.waitForLoadState('networkidle'); 
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: 'Auth Continue with Google' }).click();
+  await page.waitForTimeout(8000);
+  await page.getByRole('link', { name: buttonName }).click();
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: '+ New Chat' }).click();
+  await page.waitForLoadState('networkidle');
+});
